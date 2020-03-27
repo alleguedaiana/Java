53dec3a041ae9b5c7e6a55147a7746dcd57ac537
alleguedaiana <daianaallegue@gmail.com>
2020-03-27 20:18:58 -0300

    Lazy push

---
+++ src/ejercicios/BucleI.java
@@ -0,0 +1,25 @@
+package ejercicios;
+
+import javax.swing.JOptionPane;
+
+public class BucleI {
+
+	public static void main(String[] args) {
+		String contraseña="1234";
+		String psw="";
+		
+			while(contraseña.equals(psw)==false)
+			{
+				psw= JOptionPane.showInputDialog("Ingrese su contraseña");
+				
+				if(contraseña.equals(psw)==false)
+				{
+					psw=JOptionPane.showInputDialog("Contraseña incorrecta. Ingrese nuevamente");
+				}
+			}
+			
+			System.out.println("Contraseña correcta");
+
+	}
+
+}
